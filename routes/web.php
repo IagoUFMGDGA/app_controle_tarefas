e9030b1c4ce63e2832f0a21b698ee62004c66b2b
IagoUFMGDGA <iagoUFMG@gmail.com>
2022-08-12 16:16:45 -0300

    Criando rota que retorna uma instância da classe de notificação para o framework Laravel interpretá-la e renderizar a view contida em build

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Mail\MensagemTesteMail;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -20,4 +20,9 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
 Route::resource('/tarefa', 'App\Http\Controllers\TarefaController');
+
+Route::get('mensagem-teste', function (){
+    return new MensagemTesteMail();
+});
