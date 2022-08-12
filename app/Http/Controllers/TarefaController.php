bd76702ad8ebf4fe27ead1da73a0b258fa22c6e5
IagoUFMGDGA <iagoUFMG@gmail.com>
2022-08-12 14:01:43 -0300

    Verificando login com a classe Auth

--- app/Http/Controllers/TarefaController.php
+++ app/Http/Controllers/TarefaController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Tarefa;
 use Illuminate\Http\Request;
-
+use Illuminate\Support\Facades\Auth;
 class TarefaController extends Controller
 {
 
@@ -20,10 +20,10 @@ class TarefaController extends Controller
     public function index()
     {
         
-        if(auth()->check()){
-            $id = auth()->user()->id;
-            $name = auth()->user()->name;
-            $email = auth()->user()->email;
+        if(Auth::check()){
+            $id = Auth::user()->id;
+            $name = Auth::user()->name;
+            $email = Auth::user()->email;
             return "| ID: $id | Nome: $name | E-mail: $email |";
         }else{
             return 'Você não está logado no sistema';
