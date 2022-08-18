0ca9197594767ad397e1ce167b412dfb1b398b8b
IagoUFMGDGA <iagoUFMG@gmail.com>
2022-08-18 16:27:42 -0300

    Implementando paginação manualmente mas com layout do bootstrap

--- resources/views/tarefa/index.blade.php
+++ resources/views/tarefa/index.blade.php
@@ -24,6 +24,23 @@
                                     <td>{{ date('d/m/Y', strtotime( $tarefa['data_limite_conclusao'] )) }}</td>
                                 </tr>
                             @endforeach
+                            <nav aria-label="Page navigation example">
+                                <ul class="pagination">
+                                    <li class="page-item">
+                                        <a class="page-link" href="{{ $tarefas->previousPageUrl()}}"> Voltar </a>    
+                                    </li>
+                                    
+                                    @for( $i = 1; $i <= $tarefas->lastPage(); $i++ )
+                                        <li class="page-item {{ $tarefas->currentPage() == $i? 'active' : ''}} ">
+                                            <a class="page-link" href="{{ $tarefas->url($i) }}">{{ $i }}</a>
+                                        </li>
+                                    @endfor
+
+                                    <li class="page-item">
+                                        <a class="page-link" href="{{ $tarefas->nextPageUrl() }}">  Avançar </a>
+                                    </li>
+                                </ul>
+                            </nav>
                         </tbody>
                     </table>
                 </div>
